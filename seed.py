a65f1aad5bc23efdef7183f4a6083f4489690442
Oliver <oliver@MSI>
2023-08-11 13:05:46 -0700

    updated seed

--- seed.py
+++ seed.py
@@ -11,14 +11,3 @@ c4 = Cupcake(flavor="red velvet", size="small", rating=5.0)
 
 db.session.add_all([c1, c2, c3, c4])
 db.session.commit()
-
-CUPCAKE_DATA = {
-    "flavor": "TestFlavor",
-    "size": "TestSize",
-    "rating": 5.0,
-    "image": "http://test.com/cupcake.jpg"
-}
-
-cupcake = Cupcake(**CUPCAKE_DATA)
-db.session.add(cupcake)
-db.session.commit()
