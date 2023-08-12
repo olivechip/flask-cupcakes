9c2fede41e63602e5a09e1e39ae703e02cb4086c
Oliver <oliver@MSI>
2023-08-12 13:09:05 -0700

    added front-end delete function

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This application acts as a JSON API for cupcakes. Must create a PostgreSQL database named "cupcakes".
 
-app.py holds request and view routes (main application);
-models.py holds the main Cupcake model and database functions;
-seed.py holds sample data;
-various html templates use jinja to manipulate and display data from routes/view functions from app.py
+-app.py holds request and view routes (main application);
+-models.py holds the main Cupcake model and database functions;
+-seed.py holds sample data;
+-various html templates use jinja to manipulate and display data from routes/view functions from app.py
--- static/app.js
+++ static/app.js
@@ -1,8 +1,8 @@
 function get_cupcake_html(cupcake){
     return `
         <div data-cupcake_id="${cupcake.id}">
-            <li>${cupcake.flavor} // ${cupcake.size} // ${cupcake.rating}</li>
-            <img src="${cupcake.image}">
+            <li>${cupcake.flavor} // ${cupcake.size} // ${cupcake.rating} // <button class="delete">X</button></li>
+            <img src="${cupcake.image}" alt="${cupcake.flavor} cupcake">
         </div>
     `;
 }
@@ -26,13 +26,21 @@ function get_cupcake_form_data(){
     return data;
 }
 
+function apply_delete_func(){
+    $('.delete').on('click', function(e){
+        id = $(this).parent().parent()[0].dataset.cupcake_id;
+        delete_cupcake(id);
+    })
+}
+
 async function show_cupcakes(){
     const res = await axios.get("/api/cupcakes");
     const cc = res.data.cupcakes;
     for(let c of cc){
         let new_cc = get_cupcake_html(c);
         $("#cc_list").append(new_cc);
-    } 
+    }
+    apply_delete_func();
 }
 
 async function add_cupcake(){
@@ -42,6 +50,12 @@ async function add_cupcake(){
 
     new_cc = get_cupcake_html(cupcake_obj);
     $("#cc_list").append(new_cc);
+    apply_delete_func();
+}
+
+async function delete_cupcake(id){
+    const res = await axios.delete(`/api/cupcakes/${id}`);
+    $(`div[data-cupcake_id=${id}]`).remove();
 }
 
 $(show_cupcakes);
@@ -51,4 +65,5 @@ $('form').on('submit', function(e){
     add_cupcake();
     $(this).trigger('reset');
 })
+
 
